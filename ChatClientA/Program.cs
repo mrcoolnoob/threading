9914ecf65566b7e496366aee015ab62b8669a45b
Daud Ahmed <daudahmed870@gmail.com>
2023-04-16 18:04:42 +0500

    initial commit

---
+++ ChatClientA/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using ChatClientA;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Socket socket = new Socket();
+        Thread sender = new Thread(socket.SendPacket);
+        Thread receiver = new Thread(socket.ListeningServer);
+
+        receiver.Start();
+
+        while (true)
+        {
+            Console.WriteLine("Enter packet #: ");
+            var num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    if (receiver.IsAlive) {
+                        socket.Listen = false;
+                        Console.WriteLine("Waiting for receiver thread to finish");
+                        receiver.Join();
+                    }
+                    Console.WriteLine("Sender started");
+                    sender = new Thread(socket.SendPacket);
+                    sender.Start();
+                    Console.WriteLine("Waiting for sender to complete");
+                    sender.Join();
+                    Console.WriteLine("Sender thread execution completed");
+                    socket.Listen = true;
+                    receiver = new Thread(socket.ListeningServer);
+                    receiver.Start();
+                    Console.WriteLine("Receiver started");
+                    break;
+                case 2:
+
+                    break;
+                default:
+                    break;
+            }
+            if (num == 9) break;
+        }
+
+    }
+}
---
+++ ChatClientA/Socket.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClientA
+{
+	public class Socket
+	{
+		UdpClient udpClient;
+		IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 2000);
+        private readonly object listenLock = new object();
+        private bool listen = true;
+
+        public bool Listen
+        {
+            get
+            {
+                lock (listenLock)
+                {
+                    return listen;
+                }
+            }
+            set
+            {
+                lock (listenLock)
+                {
+                    listen = value;
+                }
+            }
+        }
+
+        public Socket()
+		{
+			udpClient = new UdpClient(1000); // localport
+			udpClient.Connect(endPoint); // remoteport
+            udpClient.Client.ReceiveTimeout = 100;
+        }
+
+        public void SendPacket()
+		{
+			try
+			{
+                udpClient.Client.ReceiveTimeout = 10000;
+                byte[] msg = Encoding.UTF8.GetBytes("Hey");
+				udpClient.Send(msg);
+				byte[] ack = udpClient.Receive(ref endPoint);
+                Console.WriteLine("Acknowledgement received");
+                Console.WriteLine(Encoding.ASCII.GetString(ack));
+			} catch (Exception e)
+			{
+				Console.WriteLine("Acknowledgement not receive");
+			}
+
+        }
+
+		public void ReceivePacket()
+		{
+			byte[] msg = udpClient.Receive(ref endPoint);
+			Console.WriteLine(Encoding.ASCII.GetString(msg));
+		}
+
+		public void ListeningServer()
+		{
+			while(Listen)
+			{
+				try
+				{
+                    byte[] msg = udpClient.Receive(ref endPoint);
+                    Console.WriteLine(Encoding.ASCII.GetString(msg));
+                    Console.WriteLine("listening");
+                }
+				catch (Exception ex)
+				{
+					continue;
+				}
+
+            }
+			Console.WriteLine("Receiver thread last line");
+		}
+	}
+}
+
